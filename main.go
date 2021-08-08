e58e885ba1cc2dc2b7b4ab7615b32b5b90738b7b
Justin Beausoleil <justin.beausoleil@fivetran.com>
2021-08-08 16:48:52 -0600

    Instructor answer.

--- main.go
+++ main.go
@@ -2,23 +2,18 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 type evenOddOutput []string
 
-func main () {
+func main() {
 	numbers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	resultArray := evenOddOutput{}
 
 	for _, number := range numbers {
-		if number % 2 == 0 {
-			resultArray = append(resultArray, strconv.Itoa(number)+" is even")
+		if number%2 == 0 {
+			fmt.Println(number, "is even")
 		} else {
-			resultArray = append(resultArray, strconv.Itoa(number)+" is odd")
+			fmt.Println(number, "is odd")
 		}
-	}
-	for _, result := range resultArray {
-		fmt.Println(result)
 	}
 }
